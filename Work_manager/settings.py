a2a3f3130638edb8a8b1385d17450a773ce56230
Arthurisme <is@live.ca>
2015-09-06 21:56:06 -0400

    ch6-begin

--- Work_manager/settings.py
+++ Work_manager/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TasksManager',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -89,3 +90,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, '../TasksManager/static/'),)
